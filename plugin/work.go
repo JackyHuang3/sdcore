7b6535826b92e2e0ddbbb949f60945c20946bba2
rs <refitself@163.com>
2022-02-23 23:44:41 +0800

    updte api interface for sdcore

--- plugin/work.go
+++ plugin/work.go
@@ -14,8 +14,8 @@ func SDDecrypt(data []byte) []byte {
 	return sdcore.SDDecrypt(data)
 }
 
-func SDPush(selfID string, recvIds []string, data []byte, isFile bool) (bool, error) {
-	return sdcore.SDPush(selfID, recvIds, data, isFile)
+func SDPush(selfID, fileName string, data []byte) (bool, error) {
+	return sdcore.SDPush(selfID, fileName, data)
 }
 
 func SDPull(selfID string) (bool, []string, error) {
